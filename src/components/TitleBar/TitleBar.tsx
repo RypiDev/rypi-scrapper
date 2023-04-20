9652ec9122e72d7e4662f1ad1902304311fa4ae4
Walidoux <ma.walidkorchi@icloud.com>
2023-04-20 15:56:29 +0000

    refactor(components): add window dragging + remove resize

--- src/components/TitleBar/TitleBar.tsx
+++ src/components/TitleBar/TitleBar.tsx
@@ -5,31 +5,33 @@ import { Image } from '../Image'
 
 export const TitleBar: React.FC = () => {
   return (
-    <nav className='flex h-[37.5px] items-center justify-between bg-[#1f1f1f] text-white'>
-      <Image src='/Logo.svg' size={60} className='ml-3 p-1' />
+    <nav className='flex w-full h-[40px] items-center justify-between bg-[#1f1f1f] text-white'>
+      <div
+        className='w-full select-none'
+        onMouseDown={async () => {
+          return await appWindow.startDragging()
+        }}>
+        <Image src='/Logo.svg' size={60} className='ml-3 p-1' />
+      </div>
 
       <ul className='flex h-full'>
         <li
+          className='grid h-full w-14 cursor-pointer place-items-center transition-colors duration-[10ms] hover:bg-[#2a2a2a]'
           onClick={async () => {
             return await appWindow.minimize()
-          }}
-          className='grid h-full w-14 cursor-pointer place-items-center transition-colors duration-[10ms] hover:bg-[#2a2a2a]'>
+          }}>
           <Minus />
         </li>
-        <li
-          onClick={async () => {
-            return (await appWindow.isMaximized())
-              ? await appWindow.unmaximize()
-              : await appWindow.maximize()
-          }}
-          className='grid h-full w-14 cursor-pointer place-items-center transition-colors duration-[10ms] hover:bg-[#2a2a2a]'>
+
+        <li className='grid h-full w-14 place-items-center cursor-not-allowed opacity-40'>
           <Maximize size={20} />
         </li>
+
         <li
+          className='grid h-full w-14 cursor-pointer place-items-center transition-colors duration-[10ms] hover:bg-red-500'
           onClick={async () => {
             return await appWindow.close()
-          }}
-          className='grid h-full w-14 cursor-pointer place-items-center transition-colors duration-[10ms] hover:bg-red-500'>
+          }}>
           <X />
         </li>
       </ul>
