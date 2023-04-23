63d9921dd53298f4f7c29601f3882fc92bde07c6
Walidoux <ma.walidkorchi@icloud.com>
2023-04-23 20:15:23 +0100

    refactor(types/Endpoint): add parser xml return type

--- src/types/Endpoint.d.ts
+++ src/types/Endpoint.d.ts
@@ -1,5 +1,9 @@
+import type { XMLParser } from 'fast-xml-parser'
+
 export type GameEndPointsTypes = Array<{
   src: string
   convert?: 'TXT' | 'XML'
   fileName: string
 }>
+
+export type IXML = ReturnType<XMLParser['parse']>
