069a37b55072008ffcd06039945552a1953e0ada
Walidoux <ma.walidkorchi@icloud.com>
2023-04-23 23:29:02 +0100

    fix(tools): fs-path for parseData

--- src/tools/parseData.ts
+++ src/tools/parseData.ts
@@ -1,9 +1,12 @@
 import { createDir, exists, writeTextFile } from '@tauri-apps/api/fs'
 
+import { Convertion } from '../config/Convertion'
+
 export const parseData = async (path: string, fileName: string, fileContent: string | object): Promise<void> => {
   const fileDir = path.concat(`/${fileName}.json`)
 
-  if (!(await exists(path))) await createDir(path)
+  if (!(await exists(Convertion.gamedataDir))) await createDir(Convertion.gamedataDir)
+  if (!(await exists(Convertion.gamedataConfigDir))) await createDir(Convertion.gamedataConfigDir)
 
   await writeTextFile(fileDir, typeof fileContent === 'object' ? JSON.stringify(fileContent) : fileContent)
 }
